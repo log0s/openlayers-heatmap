63e27bb16c4bd4aa0d9ada35d068037aaaf2fe30
Ryan Herman <ryan@tomnod.com>
2014-10-17 13:25:55 -0600

    Add Heatmap.LayerGroup functionality

--- HeatmapLayer.js
+++ HeatmapLayer.js
@@ -4,6 +4,9 @@
  *
  */
 
+ // Modified to allow layer groups with different colors by Ryan Herman <https://github.com/log0s/>
+ // Now requires color.js for parsing input colors into gradient stops
+
 Heatmap = {};
 
 /**
@@ -295,4 +298,273 @@ Heatmap.Layer = OpenLayers.Class(OpenLayers.Layer, {
 
   CLASS_NAME: 'Heatmap.Layer'
 
+});
+
+// Sub layer within a LayerGroup, contains points of a single type
+Heatmap._SubLayer = OpenLayers.Class(Heatmap.Layer, {
+  // ID of parent layer group canvas, used for events
+  parentCanvasId: null,
+
+  // Custom gradient stops based off input color
+  gradientStops: null,
+
+  // Contstructor: Heatmap.SubLayer
+  // Used internally by Heatmap.LayerGroup for tracking points of a single type
+  initialize: function(name, points, options, canvasId) {
+    OpenLayers.Layer.prototype.initialize.apply(this, arguments);
+    this.points = [];
+    this.cache = {};
+    this.canvas = document.createElement('canvas');
+    this.parentCanvasId = canvasId;
+    this.defaultRadius = 20;
+    this.defaultIntensity = 0.2;
+    this.defaultGradientStops = {
+      0.00: 0xffffff00,
+      0.10: 0x99e9fdff,
+      0.20: 0x00c9fcff,
+      0.30: 0x00e9fdff,
+      0.30: 0x00a5fcff,
+      0.40: 0x0078f2ff,
+      0.50: 0x0e53e9ff,
+      0.60: 0x4a2cd9ff,
+      0.70: 0x890bbfff,
+      0.80: 0x99019aff,
+      0.90: 0x990664ff,
+      0.99: 0x660000ff,
+      1.00: 0x000000ff
+    };
+
+    if (options && options.baseColor) {
+      this.baseColor = options.baseColor;
+      this._generateGradientStops(options.baseColor);
+    }
+    this._addPoints(points);
+    this.setGradientStops(this.gradientStops || this.defaultGradientStops);
+  },
+
+  moveTo: function(bounds, zoomChanged, dragging) {
+
+    OpenLayers.Layer.prototype.moveTo.apply(this, arguments);
+
+    // The code is too slow to update the rendering during dragging.
+    if (dragging)
+      return;
+
+    // Pick some point on the map and use it to determine the offset
+    // between the map's 0,0 coordinate and the layer's 0,0 position.
+    var someLoc = new OpenLayers.LonLat(0,0);
+    var offsetX = this.map.getViewPortPxFromLonLat(someLoc).x -
+                  this.map.getLayerPxFromLonLat(someLoc).x;
+    var offsetY = this.map.getViewPortPxFromLonLat(someLoc).y -
+                  this.map.getLayerPxFromLonLat(someLoc).y;
+
+    this.canvas.width = this.map.getSize().w;
+    this.canvas.height = this.map.getSize().h;
+
+    var ctx = this.canvas.getContext('2d');
+
+    ctx.save(); // Workaround for a bug in Google Chrome
+    ctx.fillStyle = 'transparent';
+    ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    ctx.restore();
+
+    for (var i in this.points) {
+
+      var src = this.points[i];
+      var rad = src.radius || this.defaultRadius;
+      var int = src.intensity || this.defaultIntensity;
+      var pos = this.map.getLayerPxFromLonLat(src.lonlat);
+      var x = pos.x - rad + offsetX;
+      var y = pos.y - rad + offsetY;
+
+      if (!this.cache[int]) {
+        this.cache[int] = {};
+      }
+
+      if (!this.cache[int][rad]) {
+        var grd = ctx.createRadialGradient(rad, rad, 0, rad, rad, rad);
+        grd.addColorStop(0.0, 'rgba(0, 0, 0, ' + int + ')');
+        grd.addColorStop(1.0, 'transparent');
+        this.cache[int][rad] = grd;
+      }
+
+      ctx.fillStyle = this.cache[int][rad];
+      ctx.translate(x, y);
+      ctx.fillRect(0, 0, 2 * rad, 2 * rad);
+      ctx.translate(-x, -y);
+    }
+
+    var dat = ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+    var dim = this.canvas.width * this.canvas.height * 4;
+    var pix = dat.data;
+
+    for (var p = 0; p < dim; /* */) {
+      var a = pix[ p + 3 ] * 4;
+      pix[ p++ ] = this.gradient[ a++ ];
+      pix[ p++ ] = this.gradient[ a++ ];
+      pix[ p++ ] = this.gradient[ a++ ];
+      pix[ p++ ] = this.gradient[ a++ ];
+    }
+
+    ctx.putImageData(dat, 0, 0);
+
+    // Unfortunately OpenLayers does not currently support layers that
+    // remain in a fixed position with respect to the screen location
+    // of the base layer, so this puts this layer manually back into
+    // that position using one point's offset as determined earlier.
+    this.canvas.style.left = (-offsetX) + 'px';
+    this.canvas.style.top = (-offsetY) + 'px';
+
+    // // Trigger event on parent canvas so layer group knows to add the sub-canvas data to the main canvas
+    // var event = new CustomEvent('layerDrawn', { 
+    //   detail: {
+    //     layerName: this.name
+    //   } 
+    // });
+    // var parentCanvas = document.getElementById(this.parentCanvasId);
+    // parentCanvas.dispatchEvent(event);
+  },
+
+  // Internal method to parse and add all passed points
+  _addPoints: function(points) {
+    points.forEach(function(point) {
+      this.addSource(new Heatmap.Source(point));
+    }, this);
+  },
+
+  _generateGradientStops: function(baseColor) {
+    var baseHex = Color(baseColor).hexString().replace(/#/, '');
+
+    this.gradientStops = {
+      0.00: parseInt(baseHex + '00', 16),
+      1.00: parseInt(baseHex + '88', 16)
+    };
+  },
+
+  CLASS_NAME: 'Heatmap._SubLayer'
+});
+
+Heatmap.LayerGroup = OpenLayers.Class(Heatmap.Layer, {
+  // Default gradient settings
+  defaultGradientStops: null,
+
+  // // Layers within the group, used internally for tracking
+  // _layers: null,
+
+  // List of layers, used to add layers to map
+  layers: null,
+
+  // Constructor: Heatmap.LayerGroup
+  // Used to render multiple layers with different parameters in single canvas
+  // Pass layer name, OL options, and array of objects with (at minimum) source points and options if desired
+  // REQUIRED: Name and layers
+  // USAGE:
+  // var heatLayer = new Heatmap.LayerGroup('HeatLayer', {}, [
+  //   {
+  //     points: [ OpenLayers.LonLat, OpenLayers.LonLat ],
+  //     options: {
+  //       baseColor: '#fff'
+  //     }
+  //   },
+  //   {
+  //    points: [],
+  //    options: {}
+  //   }
+  // ]);
+  initialize: function(name, options, layers) {
+    OpenLayers.Layer.prototype.initialize.apply(this, arguments);
+    this.points = [];
+    this.layers = [];
+    // this._layers = {};
+    this.cache = {};
+    this.canvas = document.createElement('canvas');
+    this.canvas.style.position = 'absolute';
+    this.canvas.id = 'heat-layer-group-' + name;
+
+    // Allow blending on the main canvas in order to mix multiple layer gradients together
+    var context = this.canvas.getContext('2d');
+    context.globalCompositeOperation = 'multiply';
+
+    // For some reason OpenLayers.Layer.setOpacity assumes there is
+    // an additional div between the layer's div and its contents.
+    var sub = document.createElement('div');
+    sub.appendChild(this.canvas);
+    this.div.appendChild(sub);
+
+    layers.forEach(function(layer, index) {
+      var layerName = this.name + index;
+      var layer = new Heatmap._SubLayer(layerName, layer.points, layer.options, this.canvas.id);
+      // this._layers[layerName] = layer;
+      this.layers.push(layer);
+    }, this);
+
+    // this.canvas.addEventListener('layerDrawn', function(event) {
+    //   this._addCanvasLayer(event);
+    // }.bind(this));
+  },
+
+  // Override Heatmap.Layer moveTo to avoid default drawing functionality
+  moveTo: function(bounds, zoomChanged, dragging) {
+    // The code is too slow to update the rendering during dragging.
+    if (dragging)
+      return;
+
+    // Pick some point on the map and use it to determine the offset
+    // between the map's 0,0 coordinate and the layer's 0,0 position.
+    var someLoc = new OpenLayers.LonLat(0,0);
+    var offsetX = this.map.getViewPortPxFromLonLat(someLoc).x -
+                  this.map.getLayerPxFromLonLat(someLoc).x;
+    var offsetY = this.map.getViewPortPxFromLonLat(someLoc).y -
+                  this.map.getLayerPxFromLonLat(someLoc).y;
+
+    this.canvas.width = this.map.getSize().w;
+    this.canvas.height = this.map.getSize().h;
+
+    var ctx = this.canvas.getContext('2d');
+
+    ctx.save(); // Workaround for a bug in Google Chrome
+    ctx.fillStyle = 'transparent';
+    ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    ctx.restore();
+
+    // Unfortunately OpenLayers does not currently support layers that
+    // remain in a fixed position with respect to the screen location
+    // of the base layer, so this puts this layer manually back into
+    // that position using one point's offset as determined earlier.
+    this.canvas.style.left = (-offsetX) + 'px';
+    this.canvas.style.top = (-offsetY) + 'px';
+
+    this._addAllLayers();
+  },
+
+  setMap: function(map) {
+    OpenLayers.Layer.prototype.setMap.apply(this, arguments);
+    // Manually add the sub layers to the map when the parent is added
+    map.addLayers(this.layers);
+  },
+
+  removeMap: function(map) {
+    OpenLayers.Layer.prototype.removeMap.apply(this, arguments);
+    // Clean up the sub layers when removing the main layer
+    this.layers.forEach(function(layer) {
+      map.removeLayer(layer);
+    });
+  },
+
+  // _addCanvasLayer: function(event) {
+  //   var context = this.canvas.getContext('2d');
+  //   var layer = this._layers[event.detail.layerName]
+
+  //   context.drawImage(layer.canvas, 0, 0, this.canvas.width, this.canvas.height);
+  // },
+
+  _addAllLayers: function() {
+    var context = this.canvas.getContext('2d');
+
+    this.layers.forEach(function(layer) {
+      context.drawImage(layer.canvas, 0, 0, this.canvas.width, this.canvas.height);
+    }, this)
+  },
+
+  CLASS_NAME: 'Heatmap.LayerGroup'
 });
